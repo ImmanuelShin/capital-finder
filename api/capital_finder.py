66488a6c993b6ab8790f18ac5bfe8a4f3fec792f
Immanuel Shin <Immanuel.shin18@gmail.com>
2024-01-29 15:28:30 -0800

    Added currency formatting

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -15,12 +15,22 @@ def get_country_info(name):
 def generate_response(country_info):
     country_name = country_info.get("name", {}).get("common", "N/A")
     country_capitals = country_info.get("capital", ["N/A"])
-    country_currencies = country_info.get("currencies", ["N/A"])
+    
+    currencies_data = country_info.get("currencies", {})
+    currencies_info = []
+
+    for currency_code, currency_data in currencies_data.items():
+        currency_name = currency_data.get("name", "N/A")
+        currency_symbol = currency_data.get("symbol", "N/A")
+        currencies_info.append((currency_name, currency_code, currency_symbol))
+
+    currencies_formatted = ", ".join([f"{name}, {code}, {symbol}" for name, code, symbol in currencies_info])
+
     country_languages = list(country_info.get("languages", {}).values())
 
     response = f"Country: {country_name}\n"
     response += f"Capitals: {', '.join(country_capitals)}\n"
-    response += f"Currencies: {', '.join(country_currencies)}\n"
+    response += f"Currencies: {', '.join(currencies_formatted)}\n"
     response += f"Languages: {', '.join(country_languages)}"
 
     return response
